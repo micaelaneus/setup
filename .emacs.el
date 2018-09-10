b0e549030911e500ea145885989e34167a2764b2
Alyssa Kwan <me@alyssackwan.name>
2018-09-10 11:11:04 -0700

    .emacs auto-save

--- .emacs.el
+++ .emacs.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(auto-save-file-name-transforms (backquote ((".*" "~/.emacs.d/auto-save/" t))))
  '(backup-directory-alist (quote ((".*" . "~/.emacs.d/backup"))))
  '(column-number-mode t)
  '(custom-safe-themes
@@ -84,7 +85,8 @@
  )
 
 (defalias 'yes-or-no-p 'y-or-n-p)
-(make-directory (concat user-emacs-directory "desktop/") :parents)
+(make-directory (expand-file-name "desktop/" user-emacs-directory) :parents)
+(make-directory (expand-file-name "auto-save/" user-emacs-directory) :parents)
 
 (require 'package)
 (let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
