2ad30fff0a3ffcce7f8e4f0d9cd8c019008233b4
Alyssa Kwan <me@alyssackwan.name>
2018-10-02 21:03:34 -0700

    stuff

--- init.sh
+++ init.sh
@@ -54,6 +54,7 @@ git clone https://github.com/alyssackwan/.password-store.git
 rm -rf .gnupg
 rm -rf .ssh
 cp -r .password-store/.gnupg .
+chmod 500 .gnupg
 cp -r .password-store/.ssh .
 
 pushd .gnupg > /dev/null
@@ -73,6 +74,7 @@ git clone git@github.com:alyssackwan/.password-store.git
 rm -rf .ssh
 rm -rf .gnupg
 ln -s .password-store/.gnupg .
+chmod 500 .gnupg
 ln -s .password-store/.ssh .
 
 pushd .gnupg > /dev/null
