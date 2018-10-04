047c988a347ea55c1a637cbf4d01e7406279f120
Alyssa Kwan <me@alyssackwan.name>
2018-10-03 21:16:37 -0700

    stuff

--- platform/uname/Darwin/bin/fa_quiet_time.sh
+++ platform/uname/Darwin/bin/fa_quiet_time.sh
@@ -10,16 +10,16 @@ trap finish_noidle EXIT
 function countdown() {
     date_end=$((`gdate +%s` + $1));
     date_next=$((`gdate +%s` + $2));
-    play -q "${HOME}/setup/var/fa/quiet_time_sound_short.mp3" &
+    play -q "/Volumes/GoogleDrive/My Drive/org_mode/fa_quiet_time_sound_short.mp3" &
     while [ "${date_end}" -ge `gdate +%s` ]; do
         echo -ne "$(gdate -u --date @$((${date_end} - `gdate +%s`)) +%H:%M:%S)\r";
         while [ "${date_next}" -le `gdate +%s` ] && [ "${date_end}" -ge `gdate +%s` ]; do
-            play -q "${HOME}/setup/var/fa/quiet_time_sound_short.mp3" &
+            play -q "/Volumes/GoogleDrive/My Drive/org_mode/fa_quiet_time_sound_short.mp3" &
             date_next=$((${date_next} + $2));
         done
         sleep 0.25
     done
-    play -q "${HOME}/setup/var/fa/quiet_time_sound_long.wav" &
+    play -q "/Volumes/GoogleDrive/My Drive/org_mode/fa_quiet_time_sound_long.wav" &
 }
 
 countdown 1800 120
