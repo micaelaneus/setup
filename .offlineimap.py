a3946e142990b764f8a5faa08139540722694de1
Micaela Neus <micaelaneus@users.noreply.github.com>
2019-01-31 13:20:07 -0800

    Delete .offlineimap.py

--- .offlineimap.py
+++
@@ -1,7 +0,0 @@
-import os
-import subprocess
-
-def remotepasseval(path):
-    HOME = os.environ['HOME']
-    path = path.format(HOME=HOME)
-    return subprocess.check_output(['gpg', '--quiet', '--batch', '--decrypt', path]).strip()
